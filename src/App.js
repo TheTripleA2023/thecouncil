aaf65d3c0b8d0f2edf84a8e7a408173bad1b6e9a
Adi Poluri <adipoluri@gmail.com>
2023-09-16 07:17:04 -0400

    Fixed Merge Conflicts with Threejs
    Note: Added overlay canvas to outermost div to let 3d objects show up properly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import * as React from "react";
 import {
 	Button,
 	ChakraProvider,
@@ -9,33 +5,62 @@ import {
 	Text,
 	Textarea,
 } from "@chakra-ui/react";
-import { AiOutlineCheck } from "react-icons/ai";
 import "./App.css";
+import { useRef, useState, Suspense} from 'react'
+import { Canvas, useFrame, useThree} from '@react-three/fiber'
+import * as React from 'react'
+import { AiOutlineCheck } from "react-icons/ai";
+import Model from "./components/Model.js"
 
-function Box(props) {
-	// This reference gives us direct access to the THREE.Mesh object
-	const ref = useRef();
-	// Hold state for hovered and clicked events
-	const [hovered, hover] = useState(false);
-	const [clicked, click] = useState(false);
-	// Subscribe this component to the render-loop, rotate the mesh every frame
-	useFrame((state, delta) => (ref.current.rotation.x += delta));
-	// Return the view, these are regular Threejs elements expressed in JSX
-	return (
-		<mesh
-			{...props}
-			ref={ref}
-			scale={clicked ? 1.5 : 1}
-			onClick={(event) => click(!clicked)}
-			onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-			onPointerOut={(event) => hover(false)}
-		>
-			<boxGeometry args={[1, 1, 1]} />
-			<meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-		</mesh>
-	);
+//Backend
+import GPTCouncil from './councilBackend/gptCouncil.js'
+
+const AIHandler = new GPTCouncil();
+
+
+//3D Components
+function CouncilMember(props, id) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+
+  function pickCouncilMember() {
+    click(!clicked)
+    AIHandler.askTheCouncil("Hello, World!, Say it back")
+  }
+
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => pickCouncilMember() }
+      onPointerOver={(event) => (event.stopPropagation(), hover(true))}
+      onPointerOut={(event) => hover(false)}>
+      <Suspense fallback={null}>
+            <Model pose={4} position={[0, 0, 0]} />
+      </Suspense>
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
 }
 
+function Shadows(props) {
+  const { viewport } = useThree()
+  return (
+    <mesh receiveShadow scale={[viewport.width, viewport.height, 1]} {...props}>
+      <planeGeometry />
+      <shadowMaterial transparent opacity={0.5} />
+    </mesh>
+  )
+}
+
+
+
+//2D Components
 function CouncilCard(props) {
 	return (
 		<div className="council-card">
@@ -51,6 +76,7 @@ function CouncilCard(props) {
 export default function App() {
 	const [pageStage, setPageStage] = useState(0);
 	const [inputValue, setInputValue] = useState("");
+  const ref = useRef()
 
 	const handleSubmit = () => {
 		setPageStage(1);
@@ -63,7 +89,7 @@ export default function App() {
 
 	return (
 		<ChakraProvider>
-			<div className="body">
+			<div ref={ref} className="container">
 				{/* PAGE STAGE 1 - HOMEPAGE */}
 				{pageStage === 0 && (
 					<div className="homepage-content">
@@ -170,8 +196,6 @@ export default function App() {
 						<Text className="council-query">{inputValue}</Text>
 					</div>
 				)}
-				{/* COMMENTED OUT THREE.JS FOR NOW */}
-				{/* <Canvas>
 					<ambientLight intensity={0.5} />
 					<spotLight
 						position={[10, 10, 10]}
@@ -183,7 +207,9 @@ export default function App() {
 					<Box position={[1.2, 0, 0]} />
 					<OrbitControls />
 				</Canvas> */}
+          eventPrefix="client">
 			</div>
 		</ChakraProvider>
 	);
 }
+export default function App() {
