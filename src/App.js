58a89526f6674dad4f1f51af25cdefc2537d1e2d
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2023-09-16 06:40:32 -0400

    CSS for homepage added

--- src/App.js
+++ src/App.js
@@ -62,87 +62,117 @@ export default function App() {
 	};
 
 	return (
-		<ChakraProvider className="body">
-			{pageStage === 0 && (
-				<div className="homepage-content">
-					<div className="homepage-title">
-						<Text className="homepage-h1">In a dilemma?</Text>
-						<Text className="homepage-h1">
-							Consult the council.
+		<ChakraProvider>
+			<div className="body">
+				{/* PAGE STAGE 1 - HOMEPAGE */}
+				{pageStage === 0 && (
+					<div className="homepage-content">
+						<div className="homepage-title">
+							<Text className="homepage-h1">In a dilemma?</Text>
+							<Text className="homepage-h1">
+								Consult
+								<span className="homepage-gradient-text">
+									the council.
+								</span>
+							</Text>
+						</div>
+
+						<div className="homepage-prompt">
+							<Input
+								className="prompt-input"
+								placeholder="Tell us what's going on"
+								style={{ width: "488px" }}
+								colorScheme="gray"
+								variant="filled"
+								_focus={{
+									borderColor: "gray", // Set the border color when the input is focused
+									textColor: "gray",
+									bg: "white",
+									boxShadow: "0 0 0 2px rgba(0, 0, 0, 0.1)", // Add a focus shadow
+								}}
+							/>
+							<Button
+								className="submit-button"
+								colorScheme="teal"
+								rightIcon={<AiOutlineCheck />}
+								variant="solid"
+								ml={2} // Add margin-left to create space between the input and button
+								onClick={handleSubmit} // Call the handleSubmit function on button click
+							>
+								OK
+							</Button>
+						</div>
+					</div>
+				)}
+				{/* PAGE STAGE 2 - THE COUNCIL */}
+				{pageStage === 1 && (
+					<div className="council-content">
+						<Text className="council-title">
+							The Council says...
 						</Text>
+						<Text className="council-query-label">Your query:</Text>
+						<Text className="council-query">{inputValue}</Text>
+						<div className="council-cards">
+							<CouncilCard
+								name="The Starfish"
+								message="Go for it! Take this opportunity to reconnect and have a conversation. First, make sure to set some boundaries, express your feelings, and listen. This could be a chance to rebuild and create something even better than before!"
+							></CouncilCard>
+							<CouncilCard
+								name="The Hammerhead"
+								message="Do not go. Entertaining the idea of going over is unwise and potentially setting yourself up for disappointment or even heartbreak. Prioritize your emotional well-being by avoiding unnecessary complications from the past."
+							></CouncilCard>
+							<CouncilCard
+								name="The Tree Frog"
+								message="Absolutely go over! This is surely a sign that they can't resist your magnetic allure. Embrace the opportunity to reignite the passion and show them what they're missing!"
+							></CouncilCard>
+							<CouncilCard
+								name="The Bear"
+								message="Listen to your instincts and consider what you truly want. If meeting your ex aligns with your personal growth and happiness, go for it. However, if it feels like a step backwards, prioritize yourself and politely decline."
+							></CouncilCard>
+						</div>
+						<div className="coucil-reply-prompt">
+							<Input
+								className="coucil-reply-prompt-input"
+								placeholder="I'm thinking about..."
+								style={{ width: "488px" }}
+							/>
+							<Button
+								className="reply-button"
+								colorScheme="teal"
+								rightIcon={<AiOutlineCheck />}
+								variant="solid"
+								ml={2} // Add margin-left to create space between the input and button
+								onClick={handleReply} // Call the handleSubmit function on button click
+							>
+								OK
+							</Button>
+						</div>
 					</div>
-
-					<div className="homepage-prompt">
-						<Input
-							className="prompt-input"
-							placeholder="Tell us what's going on"
-							style={{ width: "488px" }}
-						/>
-						<Button
-							className="submit-button"
-							colorScheme="teal"
-							rightIcon={<AiOutlineCheck />}
-							variant="solid"
-							ml={2} // Add margin-left to create space between the input and button
-							onClick={handleSubmit} // Call the handleSubmit function on button click
-						>
-							OK
-						</Button>
+				)}
+				{/* PAGE STAGE 3 - LOADING STAGE */}
+				{pageStage === 2 && (
+					<div className="council-content">
+						<Text className="council-title">
+							The Council is thinking...
+						</Text>
+						<Text className="council-query-label">Your query:</Text>
+						<Text className="council-query">{inputValue}</Text>
 					</div>
-				</div>
-			)}
-
-			{pageStage === 1 && (
-				<div className="council-content">
-					<Text className="council-title">The Council says...</Text>
-					<Text className="council-query-label">Your query:</Text>
-					<Text className="council-query">{inputValue}</Text>
-					<div className="council-cards">
-						<CouncilCard
-							name="The Starfish"
-							message="Go for it! Take this opportunity to reconnect and have a conversation. First, make sure to set some boundaries, express your feelings, and listen. This could be a chance to rebuild and create something even better than before!"
-						></CouncilCard>
-						<CouncilCard
-							name="The Hammerhead"
-							message="Do not go. Entertaining the idea of going over is unwise and potentially setting yourself up for disappointment or even heartbreak. Prioritize your emotional well-being by avoiding unnecessary complications from the past."
-						></CouncilCard>
-						<CouncilCard
-							name="The Tree Frog"
-							message="Absolutely go over! This is surely a sign that they can't resist your magnetic allure. Embrace the opportunity to reignite the passion and show them what they're missing!"
-						></CouncilCard>
-						<CouncilCard
-							name="The Bear"
-							message="Listen to your instincts and consider what you truly want. If meeting your ex aligns with your personal growth and happiness, go for it. However, if it feels like a step backwards, prioritize yourself and politely decline."
-						></CouncilCard>
-					</div>
-					<div className="coucil-reply-prompt">
-						<Input
-							className="coucil-reply-prompt-input"
-							placeholder="I'm thinking about..."
-							style={{ width: "488px" }}
-						/>
-						<Button
-							className="reply-button"
-							colorScheme="teal"
-							rightIcon={<AiOutlineCheck />}
-							variant="solid"
-							ml={2} // Add margin-left to create space between the input and button
-							onClick={handleReply} // Call the handleSubmit function on button click
-						>
-							OK
-						</Button>
-					</div>
-				</div>
-			)}
-
-			<Canvas>
-				<ambientLight intensity={0.5} />
-				<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-				<pointLight position={[-10, -10, -10]} />
-				<Box position={[-1.2, 0, 0]} />
-				<Box position={[1.2, 0, 0]} />
-				<OrbitControls />
-			</Canvas>
+				)}
+				{/* COMMENTED OUT THREE.JS FOR NOW */}
+				{/* <Canvas>
+					<ambientLight intensity={0.5} />
+					<spotLight
+						position={[10, 10, 10]}
+						angle={0.15}
+						penumbra={1}
+					/>
+					<pointLight position={[-10, -10, -10]} />
+					<Box position={[-1.2, 0, 0]} />
+					<Box position={[1.2, 0, 0]} />
+					<OrbitControls />
+				</Canvas> */}
+			</div>
 		</ChakraProvider>
 	);
 }
